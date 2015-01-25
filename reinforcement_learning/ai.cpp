41fdf81282f38d21446587ffafc53a0a80db6792
Anton Golov <jesyspa@gmail.com>
2015-01-25 21:00:05 +0100

    Improved AI.
    Now I can't beat it anymore.

--- reinforcement_learning/ai.cpp
+++ reinforcement_learning/ai.cpp
@@ -3,22 +3,29 @@
 #include <random>
 #include <iostream>
 
-AI::AI() : gen(std::random_device{}()) {}
+AI::AI() : last_move{}, gen(std::random_device{}()) {}
 
 Pos AI::get_move(Board const& board) {
-    std::uniform_real_distribution<> rdist(0, 1);
-    Pos move;
-    if (rdist(gen) < 0.2)
-        move = play_exploratory(board);
-    else
-        move = play_best(board);
-    states.push_back(board);
+    update_last_move(board);
+    auto move = choose_move(board);
+
+    explore_chance *= explore_step;
+
     Board local = board;
     local.play(move, piece);
-    states.push_back(local);
+    last_move = std::make_pair(local, board);
+
     print("[Reinforcement AI] I shall play... ", move);
     print("[Reinforcement AI] This move has value ", phase_data[local], '.');
     return move;
+}
+
+Pos AI::choose_move(Board const& board) {
+    std::uniform_real_distribution<> rdist(0, 1);
+    if (rdist(gen) < explore_chance)
+        return play_exploratory(board);
+    else
+        return play_best(board);
 }
 
 Pos AI::play_exploratory(Board const& board) {
@@ -53,33 +60,37 @@ Pos AI::play_best(Board const& board) {
     return best;
 }
 
-void AI::update_intermediate(Board board) {
-    for (auto state : states) {
-        phase_data[state] = (phase_data[state] + phase_data[board]) / 2;
-        board = state;
-    }
+void AI::update_last_move(Board const& board) {
+    if (!last_move)
+        return;
+    auto before = last_move->first;
+    auto after = last_move->second;
+    phase_data[after] += alpha * (phase_data[board] - phase_data[after]);
+    phase_data[before] += alpha * (phase_data[board] - phase_data[before]);
+    alpha *= alpha_step;
 }
 
 void AI::note_new_game() {
     games += 1;
-    states.clear();
+    last_move = {};
+    explore_chance = base_explore_chance;
 }
 
 void AI::note_victory(Board const& board) {
     wins += 1;
     phase_data[board] = 1;
-    update_intermediate(board);
+    update_last_move(board);
 }
 
 void AI::note_defeat(Board const& board) {
     losses += 1;
-    phase_data[board] = -2;
-    update_intermediate(board);
+    phase_data[board] = -1;
+    update_last_move(board);
 }
 
 void AI::note_draw(Board const& board) {
-    phase_data[board] = -1;
-    update_intermediate(board);
+    phase_data[board] = 0;
+    update_last_move(board);
 }
 
 void AI::print_info() const {
--- reinforcement_learning/ai.hpp
+++ reinforcement_learning/ai.hpp
@@ -2,6 +2,8 @@
 #include "board.hpp"
 #include "position.hpp"
 #include "phase_board.hpp"
+#include <boost/optional.hpp>
+#include <utility>
 #include <vector>
 #include <random>
 
@@ -18,15 +20,23 @@ struct AI {
     void print_info() const;
 
 private:
+    double base_explore_chance = 0.8;
+    double explore_chance = 0.8;
+    double explore_step = 0.5;
+    double alpha = 0.5;
+    double alpha_step = 0.999999;
+
     int games = 0;
     int wins = 0;
     int losses = 0;
     PhaseBoard phase_data;
-    std::vector<Board> states;
+    boost::optional<std::pair<Board, Board>> last_move;
     std::mt19937 gen;
+
+    Pos choose_move(Board const& board);
 
     Pos play_exploratory(Board const& board);
     Pos play_best(Board const& board);
 
-    void update_intermediate(Board board);
+    void update_last_move(Board const& board);
 };
